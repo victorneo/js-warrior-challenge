3c108131a70511b2b1a44572645c89d8956b3545
wlcharlie <wl00606352@gmail.com>
2022-11-21 21:11:46 +0800

    fullfilled test

--- warrior.js
+++ warrior.js
@@ -1,22 +1,53 @@
 class Warrior {
+  name
+  health = 100
+  mana = 100
   constructor(name) {
-    throw new Error("Not implemented")
+    if (!name) {
+      throw new Error("Who are you? Name should not be in silence.")
+    }
+
+    this.name = `${name}`
   }
 
   heal(amount) {
-    throw new Error("Not implemented")
+    //assuming heal wont go negative XD
+    if (amount <= 0) {
+      throw new Error("You got to have some number to heal, right?")
+    }
+
+    this.health = 100 - this.health <= amount ? 100 : this.health + amount
   }
 
   takeDamage(damage) {
-    throw new Error("Not implemented")
+    if (damage <= 0) {
+      throw new Error("You want blood? Do more number than 0.")
+    }
+
+    this.health = this.health <= damage ? 0 : this.health - damage
   }
 
   castMagic(manaCost) {
-    throw new Error("Not implemented")
+    if (manaCost <= 0) {
+      throw new Error("You want magic? Do more number than 0.")
+    }
+
+    const costPower = 2
+    const damage = manaCost * costPower
+
+    //determine magic is able or not
+    let flag = false
+
+    if (this.mana >= manaCost) {
+      this.mana -= manaCost
+      flag = true
+    }
+
+    return flag ? damage : 0
   }
 
   isAlive() {
-    throw new Error("Not implemented")
+    return this.health > 0
   }
 }
 
